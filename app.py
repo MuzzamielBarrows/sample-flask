5fd408059f7351eaa8373395708381cb0b2c93f1
MuzzamielBarrows <63879676+MuzzamielAbrahams@users.noreply.github.com>
2023-06-02 19:56:23 +0200

    test

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def logging():
+    print("This logging is working")
+    return 5
 
 @app.route("/")
-def hello_world():
-    print('Test')
+def main():
+    a = logging()
+    print(a)
     return render_template("index.html")
